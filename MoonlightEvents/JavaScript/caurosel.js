4367e250c5c12d82f950c0622bbe7922f840a030
Stany Hoang <82529095+phuchoang0167@users.noreply.github.com>
2025-02-15 16:54:33 +0700

    Delete MoonlightEvents directory

--- MoonlightEvents/JavaScript/caurosel.js
+++
@@ -1,8 +0,0 @@
-// caurosel
-var index = 0;
-function moveSlide() {
-    const carousel = document.getElementById('carousel');
-    index = (index + 1) % 3;
-    carousel.style.transform = `translateX(${-index * 100}%)`;
-}
-setInterval(moveSlide, 5000);
--- MoonlightEvents/JavaScript/visitor.js
+++
@@ -1,5 +0,0 @@
-// visitor
-var count = localStorage.getItem("visitCount") || 0;
-count++;
-localStorage.setItem("visitCount", count);
-document.getElementById("counter").innerText = count;
